2001fee3ab48b7e13ec77b85096e0ccace276c20
SolidifiedRay <yc3346@nyu.edu>
2023-03-05 10:43:34 -0800

    Set up initial test suites

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,17 @@
+import datetime
 from django.test import TestCase
+from django.utils import timezone
 
-# Create your tests here.
+from .models import Question
+
+class QuestionModelTests(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        time = timezone.now() + datetime.timedelta(days=1)
+        return Question.objects.create(question_text="question", pub_date=time)
+    
+    def test_questions_text(self):
+        question = Question.objects.get(id=1)
+        expected_question_text = f'{question.question_text}'
+        self.assertEquals(expected_question_text, "question")
