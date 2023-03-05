31a4faf109a5c789ab7930687eb6cff64b53f249
SolidifiedRay <yc3346@nyu.edu>
2023-03-05 10:54:01 -0800

    Black reformat

--- polls/tests.py
+++ polls/tests.py
@@ -4,14 +4,14 @@ from django.utils import timezone
 
 from .models import Question
 
-class QuestionModelTests(TestCase):
 
+class QuestionModelTests(TestCase):
     @classmethod
     def setUpTestData(cls):
         time = timezone.now() + datetime.timedelta(days=1)
         return Question.objects.create(question_text="question", pub_date=time)
-    
+
     def test_questions_text(self):
         question = Question.objects.get(id=1)
-        expected_question_text = f'{question.question_text}'
+        expected_question_text = f"{question.question_text}"
         self.assertEquals(expected_question_text, "question")
